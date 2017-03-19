05003b5c1489e1ee72c360dda0f62e518e9f3af7
amit-upadhyay-IT <youarefreak1@gmail.com>
2017-03-19 19:31:09 +0530

    another card type added

--- app/src/main/java/com/amitupadhyay/beautifulcards/MainActivity.java
+++ app/src/main/java/com/amitupadhyay/beautifulcards/MainActivity.java
@@ -3,6 +3,7 @@ package com.amitupadhyay.beautifulcards;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import it.gmariotti.cardslib.library.view.CardView;
 import it.gmariotti.cardslib.library.view.CardViewNative;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initCardSuggested();
+        initCardMayKnow();
     }
 
     /**
@@ -23,5 +25,26 @@ public class MainActivity extends AppCompatActivity {
         SuggestedCard card = new SuggestedCard(this);
         CardViewNative cardView = (CardViewNative) this.findViewById(R.id.carddemo_suggested);
         cardView.setCard(card);
+    }
+
+    /**
+     * This method builds a simple card
+     */
+    private void initCardMayKnow() {
+
+        //Create a Card
+        MayKnowCard card= new MayKnowCard(this);
+        card.setShadow(false);
+
+        //Set card in the cardView
+        CardView cardView = (CardView) this.findViewById(R.id.carddemo_MayKnow);
+        cardView.setCard(card);
+
+        MayKnowCard card2 = new MayKnowCard(this);
+        card2.addCardHeader(null);
+        card2.setShadow(true);
+        CardView mayView2 = (CardView) this.findViewById(R.id.carddemo_MayKnow2);
+        mayView2.setCard(card2);
+
     }
 }
